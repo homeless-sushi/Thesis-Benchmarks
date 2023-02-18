0a687a85875b723ccc3c22137c2b9629e492c6a7
Guido Rolle <rolleguido@gmail.com>
2023-02-18 17:00:45 +0100

    Add graphs

--- BFS/run.sh
+++ BFS/run.sh
@@ -2,9 +2,9 @@
 
 if [ $1 = BFS ]
 then
-    ./build/BfsAlgorithm Graphs/15k.txt Graphs/res.txt
+    ./build/BfsAlgorithm Graphs/v15k_e9_l.txt Graphs/res.txt
 elif [$1 = PROFILING]
 then 
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/Documents/Thesis/margot/paho.mqtt.c/build/src/
-    ./build/BfsProfiling Graphs/15k.txt Graphs/res.txt
+    ./build/BfsProfiling Graphs/v15k_e9_l.txt Graphs/res.txt
 fi
