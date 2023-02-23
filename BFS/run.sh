44f11c4899fff04acb2017d42331b89d27de7262
Guido Rolle <rolleguido@gmail.com>
2023-02-23 15:41:37 +0100

    Add boost/program_options

--- BFS/run.sh
+++ BFS/run.sh
@@ -2,7 +2,7 @@
 
 if [ $1 = BFS ]
 then
-    ./build/BfsAlgorithm Graphs/v15k_e9_l.txt Graphs/res.txt
+    ./build/BfsAlgorithm -I Graphs/v15k_e9_l.txt -O Graphs/res.txt
 elif [$1 = PROFILING]
 then 
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/Documents/Thesis/margot/paho.mqtt.c/build/src/
--- BFS/src/bfs.cc
+++ BFS/src/bfs.cc
@@ -2,6 +2,8 @@
 #include <string>
 #include <memory>
 
+#include <boost/program_options.hpp>
+
 #include "HMLSS_Graph/Graph.h"
 #include "HMLSS_Graph/GraphUtils.h"
 #include "HMLSS_Bfs/Bfs.h"
@@ -9,24 +11,39 @@
 #include "HMLSS_Bfs/BfsUtils.h"
 #include "HMLSS_Bfs/BfsCuda.h"
 
+namespace po = boost::program_options;
+
+po::options_description SetupOptions();
+
 int main(int argc, char *argv[])
 {
-    if(argc < 2){
-        std::cout << "Error: Invalid arguments!" << std::endl;
-        std::cout << "\tUsage: " << argv[0] << " graph.txt [result.txt]" << std::endl;
-        return -1;
-    }
+    po::options_description desc(SetupOptions());
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
 
-    std::string inputFileURL(argv[1]);
+
+    std::string inputFileURL(vm["input-file"].as<std::string>());
     Graph::Graph graph(GraphUtils::ReadGraphFile(inputFileURL));
 
-    BFS::Knobs* knobs = new BFS::GpuKnobs();
+    BFS::Knobs* knobs = new BFS::CpuKnobs();
     BFS::BfsResult* bfs = knobs->buildBfs(graph, 0);
     while(!bfs->kernel()){}
-    if(argc == 3){
-        BFSUtils::WriteGraphResultFile(argv[2], *bfs);
+    if(vm.count("output-file")){
+        BFSUtils::WriteGraphResultFile(vm["output-file"].as<std::string>(), *bfs);
     }
     
     delete knobs;
     delete bfs;
+}
+
+po::options_description SetupOptions()
+{
+    po::options_description desc("Allowed options");
+    desc.add_options()
+    ("help", "Display help message")
+    ("input-file,I", po::value<std::string>(), "input file with graph description")
+    ("output-file,O", po::value<std::string>(), "output file with bfs solution")
+    ;
+
+    return desc;
 }
