d018e65c35084f3c64dc0aab79ce7bb11065d0d2
Guido Rolle <rolleguido@gmail.com>
2023-02-27 12:00:34 +0100

    Rework margot profiling

--- BFS/run.sh
+++ BFS/run.sh
@@ -12,5 +12,5 @@ then
 elif [ $1 = PROFILING ]
 then 
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/Documents/Thesis/margot/paho.mqtt.c/build/src/
-    ./build/BfsProfiling Graphs/v15k_e9_l.txt Graphs/res.txt
+    ./build/BfsProfiling build/BfsAlgorithm Graphs/v15k_e9_l.txt Graphs/res.txt
 fi
--- BFS/src/profiling.cc
+++ BFS/src/profiling.cc
@@ -1,79 +1,67 @@
 #include <iostream>
 #include <string>
-#include <memory>
+#include <sstream>
 
-#include "HMLSS_Graph/Graph.h"
-#include "HMLSS_Graph/GraphUtils.h"
-#include "HMLSS_Bfs/Bfs.h"
+#include <cstdlib>
+
 #include "HMLSS_Bfs/BfsKnobs.h"
-#include "HMLSS_Bfs/BfsUtils.h"
-#include "HMLSS_Bfs/BfsCuda.h"
 
 #include <margot/margot.hpp>
 
 int main(int argc, char *argv[])
 {
-    if(argc < 2){
+    if(argc < 4){
         std::cout << "Error: Invalid arguments!" << std::endl;
-        std::cout << "\tUsage: " << argv[0] << " graph.txt [result.txt]" << std::endl;
+        std::cout << "\tUsage: " << argv[0] << "BfsProgram graph.txt res.txt" << std::endl;
         return -1;
     }
-
-    std::string inputFileURL(argv[1]);
-    Graph::Graph graph(GraphUtils::ReadGraphFile(inputFileURL));
+    std::string graph(argv[2]);
 
     margot::init();
     margot::bfs::context().manager.wait_for_knowledge(10);
 
-    unsigned int deviceNumber = 0;
-    BFS::Knobs::DEVICE device = BFS::Knobs::DEVICE::CPU;
-    unsigned int cpuThreadsExp = 0;
+    unsigned int deviceId = 0;
+    BFS::Knobs::DEVICE device = static_cast<BFS::Knobs::DEVICE>(deviceId);
     unsigned int cpuThreads = 1;
-    unsigned int gpuBlockSizeExp = 0;
+    unsigned int cpuThreadsExp = 0;
     BFS::GpuKnobs::BLOCK_SIZE gpuBlockSize = BFS::GpuKnobs::BLOCK_SIZE::BLOCK_32;
-    unsigned int gpuChunkFactorExp = 0;
+    unsigned int gpuBlockSizeExp = 0;
     BFS::GpuKnobs::CHUNK_FACTOR gpuChunkFactor = BFS::GpuKnobs::CHUNK_FACTOR::CHUNK_1;
-    unsigned int gpuEdgeOffsetsMemoryType = 0;
-    BFS::GpuKnobs::MEMORY_TYPE gpuEdgeOffsetsMemory = BFS::GpuKnobs::MEMORY_TYPE::DEVICE;
-    unsigned int gpuEdgesMemoryType = 0;
-    BFS::GpuKnobs::MEMORY_TYPE gpuEdgesMemory = BFS::GpuKnobs::MEMORY_TYPE::DEVICE;
+    unsigned int gpuChunkFactorExp = 0;
+    unsigned int gpuOffsetsMemId = 0;
+    BFS::GpuKnobs::MEMORY_TYPE gpuOffsetsMem = static_cast<BFS::GpuKnobs::MEMORY_TYPE>(gpuOffsetsMemId);
+    unsigned int gpuEdgesMemId = 0;
+    BFS::GpuKnobs::MEMORY_TYPE gpuEdgesMem = static_cast<BFS::GpuKnobs::MEMORY_TYPE>(gpuEdgesMemId);
 
     while(margot::bfs::context().manager.in_design_space_exploration())
     {
-        if(margot::bfs::update(cpuThreadsExp, deviceNumber, gpuBlockSizeExp, gpuChunkFactorExp, gpuEdgesMemoryType, gpuEdgeOffsetsMemoryType)){
-            device = static_cast<BFS::Knobs::DEVICE>(deviceNumber);
+        if(margot::bfs::update(cpuThreadsExp, deviceId, gpuBlockSizeExp, gpuChunkFactorExp, gpuEdgesMemId, gpuOffsetsMemId)){
+            device = static_cast<BFS::Knobs::DEVICE>(deviceId);
             cpuThreads = 1 << cpuThreadsExp;
             gpuBlockSize = static_cast<BFS::GpuKnobs::BLOCK_SIZE>(BFS::GpuKnobs::BLOCK_SIZE::BLOCK_32 << gpuBlockSizeExp);
             gpuChunkFactor = static_cast<BFS::GpuKnobs::CHUNK_FACTOR>(BFS::GpuKnobs::CHUNK_FACTOR::CHUNK_1 << gpuChunkFactorExp);
-            gpuEdgeOffsetsMemoryType = static_cast<BFS::GpuKnobs::MEMORY_TYPE>(gpuEdgeOffsetsMemoryType);
-            gpuEdgesMemory = static_cast<BFS::GpuKnobs::MEMORY_TYPE>(gpuEdgesMemoryType);
+            gpuOffsetsMem = static_cast<BFS::GpuKnobs::MEMORY_TYPE>(gpuOffsetsMemId);
+            gpuEdgesMem = static_cast<BFS::GpuKnobs::MEMORY_TYPE>(gpuEdgesMemId);
             margot::bfs::context().manager.configuration_applied();
         }
 
-        BFS::Knobs* knobs;
-        switch (device)
-        {
-            case BFS::Knobs::DEVICE::GPU :
-                knobs = new BFS::CpuKnobs(cpuThreads);
-                break;
-
-            default :
-                knobs = new BFS::GpuKnobs(gpuBlockSize, gpuChunkFactor, gpuEdgeOffsetsMemory, gpuEdgesMemory);
-        }
+        std::stringstream command;
+        command << "./" << argv[1];
+        command << " -I " << argv[2];
+        command << " -O " << argv[3];
+        command << " -D " << BFS::Knobs::ToString(device);
+        command << " --cpu-num-threads " << cpuThreads;
+        command << " --gpu-block-dim " << gpuBlockSize;
+        command << " --gpu-chunk-factor " << gpuChunkFactor;
+        command << " --gpu-offset-mem " << BFS::GpuKnobs::ToString(gpuOffsetsMem);
+        command << " --gpu-edge-mem " << BFS::GpuKnobs::ToString(gpuEdgesMem);
 
         margot::bfs::start_monitors();
 
-        BFS::BfsResult* bfs = knobs->buildBfs(graph, 0);
-        while(!bfs->kernel()){}
-        if(argc == 3){
-            BFSUtils::WriteGraphResultFile(argv[2], *bfs);
-        }
-        delete knobs;
-        delete bfs;
-
+        std::system(command.str().c_str());
+        
         margot::bfs::stop_monitors();
         margot::bfs::push_custom_monitor_values();
         margot::bfs::log();
-        std::this_thread::sleep_for(std::chrono::milliseconds(300));
     }
 }
