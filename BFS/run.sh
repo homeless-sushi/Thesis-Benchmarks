8f6bf9b87629ea0d5b09c085ce2c0f7b99c5d09b
Guido Rolle <rolleguido@gmail.com>
2023-04-03 13:35:44 +0200

    Remove call to system from profiling

--- BFS/run.sh
+++ BFS/run.sh
@@ -15,5 +15,5 @@ then
 elif [ $1 = PROFILING ]
 then 
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/Documents/Thesis/margot/paho.mqtt.c/build/src/
-    ./build/BfsProfiling -A build/BfsAlgorithm -I Graphs/v15k_e9_l.txt -O Graphs/res.txt
+    ./build/BfsProfiling -I Graphs/v15k_e9_l.txt -O Graphs/res.txt
 fi
--- BFS/src/profiling.cc
+++ BFS/src/profiling.cc
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <memory>
 #include <string>
-#include <sstream>
 
 #include <cstdlib>
 
 #include <boost/program_options.hpp>
 
+#include "HMLSS_Graph/Graph.h"
+#include "HMLSS_Graph/GraphUtils.h"
+#include "HMLSS_Bfs/Bfs.h"
 #include "HMLSS_Bfs/BfsKnobs.h"
+#include "HMLSS_Bfs/BfsUtils.h"
 
 #include <margot/margot.hpp>
 
@@ -36,8 +40,8 @@ int main(int argc, char *argv[])
     unsigned int gpuEdgesMemId = 0;
     BFS::GpuKnobs::MEMORY_TYPE gpuEdgesMem = static_cast<BFS::GpuKnobs::MEMORY_TYPE>(gpuEdgesMemId);
 
-    while(margot::bfs::context().manager.in_design_space_exploration())
-    {
+    while(margot::bfs::context().manager.in_design_space_exploration()){
+        
         if(margot::bfs::update(cpuThreadsExp, deviceId, gpuBlockSizeExp, gpuChunkFactorExp, gpuEdgesMemId, gpuOffsetsMemId)){
             device = static_cast<BFS::Knobs::DEVICE>(deviceId);
             cpuThreads = 1 << cpuThreadsExp;
@@ -48,20 +52,19 @@ int main(int argc, char *argv[])
             margot::bfs::context().manager.configuration_applied();
         }
 
-        std::stringstream command;
-        command << "./" << vm["bfs-program"].as<std::string>();
-        command << " -I " << vm["input-file"].as<std::string>();
-        command << " -O " << vm["output-file"].as<std::string>();
-        command << " -D " << BFS::Knobs::ToString(device);
-        command << " --cpu-num-threads " << cpuThreads;
-        command << " --gpu-block-dim " << gpuBlockSize;
-        command << " --gpu-chunk-factor " << gpuChunkFactor;
-        command << " --gpu-offset-mem " << BFS::GpuKnobs::ToString(gpuOffsetsMem);
-        command << " --gpu-edge-mem " << BFS::GpuKnobs::ToString(gpuEdgesMem);
-
         margot::bfs::start_monitors();
 
-        std::system(command.str().c_str());
+        Graph::Graph graph(GraphUtils::ReadGraphFile(vm["input-file"].as<std::string>()));
+        std::unique_ptr<BFS::Knobs> knobs( 
+            device == BFS::Knobs::DEVICE::GPU ?
+            static_cast<BFS::Knobs*>(new BFS::GpuKnobs(gpuBlockSize, gpuChunkFactor, gpuOffsetsMem, gpuEdgesMem)) :
+            static_cast<BFS::Knobs*>(new BFS::CpuKnobs(cpuThreads))
+        );
+        std::unique_ptr<BFS::BfsResult> bfs(knobs->buildBfs(graph, 0));
+        while(!bfs->kernel()){}
+        if(vm.count("output-file")){
+            BFSUtils::WriteGraphResultFile(vm["output-file"].as<std::string>(), *bfs);
+        }
         
         margot::bfs::stop_monitors();
         margot::bfs::push_custom_monitor_values();
@@ -74,7 +77,6 @@ po::options_description SetupOptions()
     po::options_description desc("Allowed options");
     desc.add_options()
     ("help", "Display help message")
-    ("bfs-program,A", po::value<std::string>(), "bfs program to profile")
     ("input-file,I", po::value<std::string>(), "input file with graph description")
     ("output-file,O", po::value<std::string>(), "output file with bfs solution")
     ;
