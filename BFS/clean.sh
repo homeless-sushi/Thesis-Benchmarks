466c34d0abf58844c5e262b9e17f858335fafb59
Guido Rolle <rolleguido@gmail.com>
2023-04-14 15:12:38 +0200

    Add clean.sh

---
+++ BFS/clean.sh
@@ -0,0 +1,8 @@
+sudo rm -rf BFSApp
+sudo rm -rf observations
+sudo rm -rf configurations
+sudo rm -rf predictions
+sudo rm -rf descriptions
+sudo rm -rf clusters
+sudo rm -rf models
+sudo rm margot.bfs.log
